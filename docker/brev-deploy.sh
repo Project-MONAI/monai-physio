81018d3be3c5bb123684fff714e1238bc4edf53a
maxofir <maxofir@nvidia.com>
2026-09-01 12:03:32 +0200

    FIX: Propagate Docker sudo fallback
    Route bundle downloads through the selected Docker access mode and deploy the verified v0.3-cu126 image.

--- docker/brev-deploy.sh
+++ docker/brev-deploy.sh
@@ -4,7 +4,7 @@
 
 set -Eeuo pipefail
 
-readonly NGC_IMAGE="nvcr.io/0569033758414229/physiomotion:v0.2-cu126"
+readonly NGC_IMAGE="nvcr.io/0569033758414229/physiomotion:v0.3-cu126"
 readonly LOCAL_IMAGE="monai-physio:tutorials"
 readonly INSTALL_DIR="${HOME}/monai-physio"
 
@@ -105,6 +105,7 @@ docker_cmd run --rm \
     -c 'cp -a /workspace/monai-physio/. /deployment/'
 
 echo "Downloading the private lung workshop bundles..."
+MONAI_PHYSIO_DOCKER_USE_SUDO="${use_sudo}" \
 MONAI_PHYSIO_IMAGE="${LOCAL_IMAGE}" \
     "${INSTALL_DIR}/docker/download-lung-bundles.sh"
 unset HF_TOKEN
