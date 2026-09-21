maximilianofir/physiotwin4d

ENH: Dockerize lung tutorial workflow

tests/test_cli_smoke.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import importlib
 from pathlib import Path
 import sys
-from typing import Any
+from typing import Any, Optional
 
 import pytest
 
@@ -19,6 +19,7 @@ CLI_MODULES = [
     "physiotwin4d.cli.download_data",
     "physiotwin4d.cli.fit_statistical_model_to_patient",
     "physiotwin4d.cli.reconstruct_highres_4d_ct",
+    "physiotwin4d.cli.view_meshes",
     "physiotwin4d.cli.visualize_pca_modes",
 ]
 
@@ -111,3 +112,122 @@ def test_convert_image_to_usd_cli_passes_fps(
     assert captured_kwargs["time_series_images"] == [fake_image]
     assert captured_kwargs["reference_image"] is fake_image
     assert captured_kwargs["frames_per_second"] == 30.0
+
+
+def test_view_meshes_cli_forwards_server_options(
+    monkeypatch: pytest.MonkeyPatch,
+    tmp_path: Path,
+) -> None:
+    """Mesh viewer CLI forwards the scene and web-server options."""
+    module = importlib.import_module("physiotwin4d.cli.view_meshes")
+    usd_file = tmp_path / "animated.usd"
+    usd_file.write_text("placeholder")
+    captured: dict[str, Any] = {}
+
+    class FakeMeshWebViewer:
+        def __init__(
+            self,
+            input_files: list[Path],
+            prim_path: str,
+            playback_fps: Optional[float],
+        ) -> None:
+            captured["input_files"] = input_files
+            captured["prim_path"] = prim_path
+            captured["playback_fps"] = playback_fps
+
+        def start(self, host: str, port: int, open_browser: bool) -> None:
+            captured["host"] = host
+            captured["port"] = port
+            captured["open_browser"] = open_browser
+
+    import physiotwin4d.mesh_web_viewer
+
+    monkeypatch.setattr(
+        physiotwin4d.mesh_web_viewer,
+        "MeshWebViewer",
+        FakeMeshWebViewer,
+    )
+    monkeypatch.setattr(
+        sys,
+        "argv",
+        [
+            "view_meshes",
+            str(usd_file),
+            "--prim-path",
+            "/Scene",
+            "--host",
+            "0.0.0.0",
+            "--port",
+            "9000",
+            "--fps",
+            "3",
+            "--no-browser",
+        ],
+    )
+
+    assert module.main() == 0
+    assert captured == {
+        "input_files": [usd_file],
+        "prim_path": "/Scene",
+        "playback_fps": 3.0,
+        "host": "0.0.0.0",
+        "port": 9000,
+        "open_browser": False,
+    }
+
+
+def test_view_meshes_cli_forwards_multiple_vtp_files(
+    monkeypatch: pytest.MonkeyPatch,
+    tmp_path: Path,
+) -> None:
+    """Viewer CLI forwards all VTP surfaces in command-line order."""
+    module = importlib.import_module("physiotwin4d.cli.view_meshes")
+    patient_file = tmp_path / "patient.vtp"
+    fitted_file = tmp_path / "fitted.vtp"
+    patient_file.write_text("placeholder")
+    fitted_file.write_text("placeholder")
+    captured: dict[str, Any] = {}
+
+    class FakeMeshWebViewer:
+        def __init__(
+            self,
+            input_files: list[Path],
+            prim_path: str,
+            playback_fps: Optional[float],
+        ) -> None:
+            captured["input_files"] = input_files
+            captured["prim_path"] = prim_path
+            captured["playback_fps"] = playback_fps
+
+        def start(self, host: str, port: int, open_browser: bool) -> None:
+            captured["host"] = host
+            captured["port"] = port
+            captured["open_browser"] = open_browser
+
+    import physiotwin4d.mesh_web_viewer
+
+    monkeypatch.setattr(
+        physiotwin4d.mesh_web_viewer,
+        "MeshWebViewer",
+        FakeMeshWebViewer,
+    )
+    monkeypatch.setattr(
+        sys,
+        "argv",
+        [
+            "view_meshes",
+            str(patient_file),
+            str(fitted_file),
+            "--no-browser",
+        ],
+    )
+
+    assert module.main() == 0
+    assert captured == {
+        "input_files": [patient_file, fitted_file],
+        "prim_path": "/World",
+        "playback_fps": None,
+        "host": "127.0.0.1",
+        "port": 8080,
+        "open_browser": False,
+    }

tutorials/tutorial_04_lung_ct_to_vtk.py
@@ -1,16 +1,22 @@
-"""
-Tutorial 4 (Lung): CT Segmentation to VTK Surfaces
+"""Tutorial 4 (Lung): CT segmentation to VTK surfaces with NV-Segment-CTMR.
 
 Purpose
 -------
-Segment one 3D CT frame into anatomical groups and save a combined VTK
-surface file. The output can be inspected directly in PyVista or used as
-input for Tutorial 5.
+Convert one CT frame into a detailed labelmap, a lung-only labelmap, and VTK
+surfaces. This exposes the image-to-anatomy boundary that participants replace
+when adapting the pipeline to another organ.
 
 Data Required
 -------------
 Full data: ``data/DirLab-4DCT/Case1Pack_T??.mha``
 Test data: ``data/test/DirLab-4DCT/Case1Pack_T??.mha``
+
+Segmentation Models
+-------------------
+This tutorial uses the shared lung segmenter, ``SegmentNVSegmentCTMRI``. Its
+weights use the NVIDIA OneWay Non-Commercial License. The output keeps the
+model's published label IDs; the lung-only labelmap retains the five lobe IDs
+and any other predicted lung labels while clearing non-lung voxels.
 """
 
 # Imports
@@ -20,28 +26,26 @@ import logging
 from pathlib import Path
 
 import itk
+import numpy as np
 import pyvista as pv
 
 from parameters_lung_ct_dirlab import LUNG_CT_DIRLAB
 
 from physiotwin4d import (
     ContourTools,
-    SegmentChestTotalSegmentator,
     TestTools,
     WorkflowConvertImageToVTK,
 )
 
 # Only run if this script is not imported as a module
 
-# nnUNetv2 (used by TotalSegmentator inside several workflows) spawns a
-# multiprocessing.Pool. On Windows the spawn start method re-imports this
-# script in each child; without the __name__ == "__main__" guard around
-# top-level work, that re-import fires the segmenter again and Python's
-# spawn-cascade detector raises RuntimeError.
+# Keep GPU segmentation and file writes below the main guard so importing this
+# tutorial for documentation or test discovery has no runtime side effects.
 if __name__ == "__main__":
     # Data directory specification
 
     project_name = "tutorial_04_lung"
+    output_prefix = "patient_nvsegmentctmri"
 
     test_mode = TestTools.running_as_test()
 
@@ -61,9 +65,7 @@ if __name__ == "__main__":
     frame_files = sorted(data_dir.glob("Case1Pack_T??.mha"))
 
     log_level = logging.INFO
-
-    segmentation_method = SegmentChestTotalSegmentator(log_level=log_level)
-    segmentation_method.set_has_academic_license(True)
+    segmentation_method = LUNG_CT_DIRLAB.segmenter_class(log_level=log_level)
 
     # Directory setup and data reading
     output_dir.mkdir(parents=True, exist_ok=True)
@@ -90,6 +92,7 @@ if __name__ == "__main__":
     # surface_reduction_rate decimates each exported VTP surface.
     result = workflow.process(
         input_image=ct_image,
+        anatomy_groups=[LUNG_CT_DIRLAB.anatomy_group],
         surface_reduction_rate=LUNG_CT_DIRLAB.surface_reduction_rate,
         extract_label_surfaces=save_label_surfaces,
     )
@@ -107,19 +110,35 @@ if __name__ == "__main__":
     surface_file = Path(
         ContourTools.save_combined_surfaces(
             combined_input,
-            str(output_dir / "patient_surfaces.vtp"),
+            str(output_dir / f"{output_prefix}_surfaces.vtp"),
         )
     )
     if save_group_surfaces:
         ContourTools.save_surfaces(
-            result["surfaces"], str(output_dir), prefix="patient"
+            result["surfaces"], str(output_dir), prefix=output_prefix
         )
     if save_label_surfaces:
         ContourTools.save_surfaces(
-            result["label_surfaces"], str(output_dir), prefix="patient"
+            result["label_surfaces"], str(output_dir), prefix=output_prefix
         )
-    labelmap_file = output_dir / "patient_labelmap.mha"
+    labelmap_file = output_dir / f"{output_prefix}_labelmap.mha"
     itk.imwrite(result["labelmap"], str(labelmap_file), compression=True)
+
+    # Keep the model's native lobe ids but clear every non-lung label. This is
+    # the labelmap participants inspect or replace for another organ.
+    lung_label_ids = np.array(
+        sorted(
+            segmentation_method.taxonomy.labels_in_group(LUNG_CT_DIRLAB.anatomy_group)
+        ),
+        dtype=np.uint16,
+    )
+    full_labelmap_arr = itk.GetArrayViewFromImage(result["labelmap"])
+    lung_labelmap = itk.GetImageFromArray(
+        np.where(np.isin(full_labelmap_arr, lung_label_ids), full_labelmap_arr, 0)
+    )
+    lung_labelmap.CopyInformation(result["labelmap"])
+    lung_labelmap_file = output_dir / f"{output_prefix}_lung_labelmap.mha"
+    itk.imwrite(lung_labelmap, str(lung_labelmap_file), compression=True)
 
     # Testing
     tt = TestTools(
@@ -138,7 +157,7 @@ if __name__ == "__main__":
             colormap="gray",
             vmin=-200,
             vmax=600,
-            overlay_mask=result["labelmap"],
+            overlay_mask=lung_labelmap,
         )
     )
 
@@ -161,5 +180,6 @@ if __name__ == "__main__":
         "result": result,
         "surface_file": surface_file,
         "labelmap_file": labelmap_file,
+        "lung_labelmap_file": lung_labelmap_file,
         "screenshots": screenshots,
     }

tutorials/tutorial_11_lung_evaluate_physicsnemo.py
@@ -46,8 +46,13 @@ PhysicsNeMo and PyTorch Geometric must be installed::
 Data Required
 -------------
   * ``data/DirLab-4DCT/<case>_T??.mha``  - the gated CT sequence
-  * ``output/tutorial_08_lung/<case>/``  - Tutorial 8 SSM surface + coefficients
+  * ``output/tutorial_08_lung/<case>/``  - Tutorial 8 fit + phase transforms
   * ``network_weights/physicsnemo_mgn_lung_motion/`` - Tutorial 9 checkpoint
+
+The Tutorial 8 anatomy is automatically refitted to the PCA domain bundled
+with the checkpoint and cached under
+``output/tutorial_08_lung_checkpoint/<case>/``. The original Tutorial 6 and 8
+outputs are not overwritten.
 
 Outputs (under ``output/tutorial_11_lung/<case>/``)
 ---------------------------------------------------
@@ -72,6 +77,7 @@ from typing import Any, Optional, cast
 
 import itk
 import pyvista as pv
+from lung_mgn_checkpoint_tools import prepare_checkpoint_compatible_lung_case
 from parameters_lung_ct_dirlab import LUNG_CT_DIRLAB
 
 from physiotwin4d import (
@@ -92,6 +98,8 @@ from physiotwin4d import (
 if __name__ == "__main__":
     # Data directory specification
     repo_root = Path(__file__).resolve().parent.parent
+    test_mode = TestTools.running_as_test()
+    output_root = LUNG_CT_DIRLAB.output_directory(test_mode)
 
     class_name = "tutorial_11_lung_evaluate_physicsnemo"
 
@@ -101,10 +109,8 @@ if __name__ == "__main__":
     # the predicted deformations carry into every other phase.
     reference_phase = "T70"
 
-    # Fitted SSM surface and PCA coefficients written by Tutorial 8 (lung).
-    test_mode = TestTools.running_as_test()
-    # Keep a test run out of the directories a full run reads and writes.
-    case_dir = LUNG_CT_DIRLAB.output_directory(test_mode) / "tutorial_08_lung" / case_id
+    # Fitted anatomy and respiratory transforms written by Tutorial 8 (lung).
+    source_case_dir = output_root / "tutorial_08_lung" / case_id
     # Weights Tutorial 9 trained, and the checkpoint epoch Tutorial 10 infers
     # with; None uses the final weights.
     model_dir = LUNG_CT_DIRLAB.mgn_weights_directory(test_mode)
@@ -130,9 +136,15 @@ if __name__ == "__main__":
     # in the wrong direction cannot hide in, and it costs one mesh read a phase.
     include_displacement_error = True
 
-    output_dir = (
-        LUNG_CT_DIRLAB.output_directory(test_mode) / "tutorial_11_lung" / case_id
+    icon_distancemap_weights_path = (
+        LUNG_CT_DIRLAB.weights_directory(test_mode)
+        / "icon_dirlab_4dct_distancemap"
+        / "icon_dirlab_4dct_distancemap_model"
+        / "checkpoints"
+        / "network_weights_final.trch"
     )
+
+    output_dir = output_root / "tutorial_11_lung" / case_id
     ground_truth_dir = output_dir / "ground_truth"
     log_level = logging.INFO
 
@@ -145,13 +157,22 @@ if __name__ == "__main__":
 
     ground_truth_dir.mkdir(parents=True, exist_ok=True)
 
-    fitted_reference_mesh_file = case_dir / f"{case_id}_ssm_surface.vtp"
-    pca_file = case_dir / f"{case_id}_ssm_pca_coefficients.json"
+    compatible_case = prepare_checkpoint_compatible_lung_case(
+        source_case_dir=source_case_dir,
+        model_dir=model_dir,
+        output_dir=(output_root / "tutorial_08_lung_checkpoint" / case_id),
+        mask_dilation_mm=LUNG_CT_DIRLAB.mask_dilation_mm,
+        distancemap_squared_max=LUNG_CT_DIRLAB.distancemap_squared_max,
+        icon_weights_path=icon_distancemap_weights_path,
+        log_level=log_level,
+    )
+    fitted_reference_mesh_file = compatible_case.reference_mesh
+    pca_file = compatible_case.pca_coefficients
     for required_file in (fitted_reference_mesh_file, pca_file):
         if not required_file.exists():
             raise FileNotFoundError(
-                f"Tutorial 8 output not found: {required_file}\n"
-                "Run tutorials/tutorial_08_lung_fit_model_to_4d_patients.py first."
+                f"Checkpoint-compatible Tutorial 8 output not found: "
+                f"{required_file}"
             )
 
     # Step 1: the cohort assembles what this case is scored against.  Every
@@ -166,7 +187,7 @@ if __name__ == "__main__":
     ground_truth = cohort.assemble_ground_truth(
         case_id=case_id,
         frame_directory=data_dir,
-        fit_directory=case_dir,
+        fit_directory=output_root / "tutorial_08_lung_checkpoint" / case_id,
         cache_directory=ground_truth_dir,
     )
 

Project-MONAI/monai-physio

tests/test_cli_smoke.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import importlib
 import sys
 from pathlib import Path
-from typing import Any
+from typing import Any, Optional
 
 import pytest
 
@@ -18,6 +18,7 @@ CLI_MODULES = [
     "monai_physio.cli.download_data",
     "monai_physio.cli.fit_statistical_model_to_patient",
     "monai_physio.cli.reconstruct_highres_4d_ct",
+    "monai_physio.cli.view_meshes",
     "monai_physio.cli.visualize_pca_modes",
 ]
 
@@ -110,3 +111,122 @@ def test_convert_image_to_usd_cli_passes_fps(
     assert captured_kwargs["time_series_images"] == [fake_image]
     assert captured_kwargs["reference_image"] is fake_image
     assert captured_kwargs["frames_per_second"] == 30.0
+
+
+def test_view_meshes_cli_forwards_server_options(
+    monkeypatch: pytest.MonkeyPatch,
+    tmp_path: Path,
+) -> None:
+    """Mesh viewer CLI forwards the scene and web-server options."""
+    module = importlib.import_module("monai_physio.cli.view_meshes")
+    usd_file = tmp_path / "animated.usd"
+    usd_file.write_text("placeholder")
+    captured: dict[str, Any] = {}
+
+    class FakeMeshWebViewer:
+        def __init__(
+            self,
+            input_files: list[Path],
+            prim_path: str,
+            playback_fps: Optional[float],
+        ) -> None:
+            captured["input_files"] = input_files
+            captured["prim_path"] = prim_path
+            captured["playback_fps"] = playback_fps
+
+        def start(self, host: str, port: int, open_browser: bool) -> None:
+            captured["host"] = host
+            captured["port"] = port
+            captured["open_browser"] = open_browser
+
+    import monai_physio.mesh_web_viewer
+
+    monkeypatch.setattr(
+        monai_physio.mesh_web_viewer,
+        "MeshWebViewer",
+        FakeMeshWebViewer,
+    )
+    monkeypatch.setattr(
+        sys,
+        "argv",
+        [
+            "view_meshes",
+            str(usd_file),
+            "--prim-path",
+            "/Scene",
+            "--host",
+            "0.0.0.0",
+            "--port",
+            "9000",
+            "--fps",
+            "3",
+            "--no-browser",
+        ],
+    )
+
+    assert module.main() == 0
+    assert captured == {
+        "input_files": [usd_file],
+        "prim_path": "/Scene",
+        "playback_fps": 3.0,
+        "host": "0.0.0.0",
+        "port": 9000,
+        "open_browser": False,
+    }
+
+
+def test_view_meshes_cli_forwards_multiple_vtp_files(
+    monkeypatch: pytest.MonkeyPatch,
+    tmp_path: Path,
+) -> None:
+    """Viewer CLI forwards all VTP surfaces in command-line order."""
+    module = importlib.import_module("monai_physio.cli.view_meshes")
+    patient_file = tmp_path / "patient.vtp"
+    fitted_file = tmp_path / "fitted.vtp"
+    patient_file.write_text("placeholder")
+    fitted_file.write_text("placeholder")
+    captured: dict[str, Any] = {}
+
+    class FakeMeshWebViewer:
+        def __init__(
+            self,
+            input_files: list[Path],
+            prim_path: str,
+            playback_fps: Optional[float],
+        ) -> None:
+            captured["input_files"] = input_files
+            captured["prim_path"] = prim_path
+            captured["playback_fps"] = playback_fps
+
+        def start(self, host: str, port: int, open_browser: bool) -> None:
+            captured["host"] = host
+            captured["port"] = port
+            captured["open_browser"] = open_browser
+
+    import monai_physio.mesh_web_viewer
+
+    monkeypatch.setattr(
+        monai_physio.mesh_web_viewer,
+        "MeshWebViewer",
+        FakeMeshWebViewer,
+    )
+    monkeypatch.setattr(
+        sys,
+        "argv",
+        [
+            "view_meshes",
+            str(patient_file),
+            str(fitted_file),
+            "--no-browser",
+        ],
+    )
+
+    assert module.main() == 0
+    assert captured == {
+        "input_files": [patient_file, fitted_file],
+        "prim_path": "/World",
+        "playback_fps": None,
+        "host": "127.0.0.1",
+        "port": 8080,
+        "open_browser": False,
+    }

tutorials/tutorial_04_lung_ct_to_vtk.py
@@ -1,16 +1,22 @@
-"""
-Tutorial 4 (Lung): CT Segmentation to VTK Surfaces
+"""Tutorial 4 (Lung): CT segmentation to VTK surfaces with NV-Segment-CTMR.
 
 Purpose
 -------
-Segment one 3D CT frame into anatomical groups and save a combined VTK
-surface file. The output can be inspected directly in PyVista or used as
-input for Tutorial 5.
+Convert one CT frame into a detailed labelmap, a lung-only labelmap, and VTK
+surfaces. This exposes the image-to-anatomy boundary that participants replace
+when adapting the pipeline to another organ.
 
 Data Required
 -------------
 Full data: ``data/TCIA-4DLung/100_HM10395/100_HM10395_g0??.nii.gz``
 Test data: ``data/test/TCIA-4DLung/100_HM10395/100_HM10395_g0??.nii.gz``
+
+Segmentation Models
+-------------------
+This tutorial uses the shared lung segmenter, ``SegmentNVSegmentCTMRI``. Its
+weights use the NVIDIA OneWay Non-Commercial License. The output keeps the
+model's published label IDs; the lung-only labelmap retains the five lobe IDs
+and any other predicted lung labels while clearing non-lung voxels.
 """
 
 # Imports
@@ -20,6 +26,7 @@ import logging
 from pathlib import Path
 
 import itk
+import numpy as np
 import pyvista as pv
 from parameters_tcia_4d_lung import TCIA_4D_LUNG
 
@@ -31,15 +38,13 @@ from monai_physio import (
 
 # Only run if this script is not imported as a module
 
-# nnUNetv2 (used by TotalSegmentator inside several workflows) spawns a
-# multiprocessing.Pool. On Windows the spawn start method re-imports this
-# script in each child; without the __name__ == "__main__" guard around
-# top-level work, that re-import fires the segmenter again and Python's
-# spawn-cascade detector raises RuntimeError.
+# Keep GPU segmentation and file writes below the main guard so importing this
+# tutorial for documentation or test discovery has no runtime side effects.
 if __name__ == "__main__":
     # Data directory specification
 
     project_name = "tutorial_04_lung"
+    output_prefix = "patient_nvsegmentctmri"
 
     test_mode = ProcessTests.running_as_test()
 
@@ -56,7 +61,6 @@ if __name__ == "__main__":
     frame_files = sorted(data_dir.glob("100_HM10395_g0??.nii.gz"))
 
     log_level = logging.INFO
-
     segmentation_method = TCIA_4D_LUNG.segmenter(test_mode, log_level=log_level)
 
     # Directory setup and data reading
@@ -84,6 +88,7 @@ if __name__ == "__main__":
     # surface_reduction_rate decimates each exported VTP surface.
     result = workflow.process(
         input_image=ct_image,
+        anatomy_groups=[TCIA_4D_LUNG.anatomy_group],
         surface_reduction_rate=TCIA_4D_LUNG.surface_reduction_rate,
         extract_label_surfaces=save_label_surfaces,
     )
@@ -101,19 +106,35 @@ if __name__ == "__main__":
     surface_file = Path(
         ProcessContours.save_combined_surfaces(
             combined_input,
-            str(output_dir / "patient_surfaces.vtp"),
+            str(output_dir / f"{output_prefix}_surfaces.vtp"),
         )
     )
     if save_group_surfaces:
         ProcessContours.save_surfaces(
-            result["surfaces"], str(output_dir), prefix="patient"
+            result["surfaces"], str(output_dir), prefix=output_prefix
         )
     if save_label_surfaces:
         ProcessContours.save_surfaces(
-            result["label_surfaces"], str(output_dir), prefix="patient"
+            result["label_surfaces"], str(output_dir), prefix=output_prefix
         )
-    labelmap_file = output_dir / "patient_labelmap.mha"
+    labelmap_file = output_dir / f"{output_prefix}_labelmap.mha"
     itk.imwrite(result["labelmap"], str(labelmap_file), compression=True)
+
+    # Keep the model's native lobe ids but clear every non-lung label. This is
+    # the labelmap participants inspect or replace for another organ.
+    lung_label_ids = np.array(
+        sorted(
+            segmentation_method.taxonomy.labels_in_group(TCIA_4D_LUNG.anatomy_group)
+        ),
+        dtype=np.uint16,
+    )
+    full_labelmap_arr = itk.GetArrayViewFromImage(result["labelmap"])
+    lung_labelmap = itk.GetImageFromArray(
+        np.where(np.isin(full_labelmap_arr, lung_label_ids), full_labelmap_arr, 0)
+    )
+    lung_labelmap.CopyInformation(result["labelmap"])
+    lung_labelmap_file = output_dir / f"{output_prefix}_lung_labelmap.mha"
+    itk.imwrite(lung_labelmap, str(lung_labelmap_file), compression=True)
 
     # Testing
     tt = ProcessTests(
@@ -132,7 +153,7 @@ if __name__ == "__main__":
             colormap="gray",
             vmin=-200,
             vmax=600,
-            overlay_mask=result["labelmap"],
+            overlay_mask=lung_labelmap,
         )
     )
 
@@ -155,5 +176,6 @@ if __name__ == "__main__":
         "result": result,
         "surface_file": surface_file,
         "labelmap_file": labelmap_file,
+        "lung_labelmap_file": lung_labelmap_file,
         "screenshots": screenshots,
     }

tutorials/tutorial_11_lung_evaluate_physicsnemo.py
@@ -40,8 +40,13 @@ the lobes and the ground truth.
 Data Required
 -------------
   * ``data/TCIA-4DLung/<case>/<case>_g0??.nii.gz``  - the gated CT sequence
-  * ``output/tutorial_08_lung/<case>/``  - Tutorial 8 SSM surface + coefficients
+  * ``output/tutorial_08_lung/<case>/``  - Tutorial 8 fit + phase transforms
   * ``network_weights/physicsnemo_mgn_lung_motion/`` - Tutorial 9 checkpoint
+
+The Tutorial 8 anatomy is automatically refitted to the PCA domain bundled
+with the checkpoint and cached under
+``output/tutorial_08_lung_checkpoint/<case>/``. The original Tutorial 6 and 8
+outputs are not overwritten.
 
 Outputs (under ``output/tutorial_11_lung/<case>/``)
 ---------------------------------------------------
@@ -66,6 +71,7 @@ from typing import Any, Optional, cast
 
 import itk
 import pyvista as pv
+from lung_mgn_checkpoint_tools import prepare_checkpoint_compatible_lung_case
 from parameters_tcia_4d_lung import TCIA_4D_LUNG
 
 from monai_physio import (
@@ -85,6 +91,8 @@ from monai_physio import (
 if __name__ == "__main__":
     # Data directory specification
     repo_root = Path(__file__).resolve().parent.parent
+    test_mode = ProcessTests.running_as_test()
+    output_root = TCIA_4D_LUNG.output_directory(test_mode)
 
     class_name = "tutorial_11_lung_evaluate_physicsnemo"
 
@@ -94,10 +102,8 @@ if __name__ == "__main__":
     # the predicted deformations carry into every other phase.
     reference_phase = "g070"
 
-    # Fitted SSM surface and PCA coefficients written by Tutorial 8 (lung).
-    test_mode = ProcessTests.running_as_test()
-    # Keep a test run out of the directories a full run reads and writes.
-    case_dir = TCIA_4D_LUNG.output_directory(test_mode) / "tutorial_08_lung" / case_id
+    # Fitted anatomy and respiratory transforms written by Tutorial 8 (lung).
+    source_case_dir = output_root / "tutorial_08_lung" / case_id
     # Weights Tutorial 9 trained, and the checkpoint epoch Tutorial 10 infers
     # with; None uses the final weights.
     model_dir = TCIA_4D_LUNG.mgn_weights_directory(test_mode)
@@ -123,7 +129,15 @@ if __name__ == "__main__":
     # in the wrong direction cannot hide in, and it costs one mesh read a phase.
     include_displacement_error = True
 
-    output_dir = TCIA_4D_LUNG.output_directory(test_mode) / "tutorial_11_lung" / case_id
+    icon_distancemap_weights_path = (
+        TCIA_4D_LUNG.weights_directory(test_mode)
+        / "icon_tcia_4dlung_distancemap"
+        / "icon_tcia_4dlung_distancemap_model"
+        / "checkpoints"
+        / "network_weights_final.trch"
+    )
+
+    output_dir = output_root / "tutorial_11_lung" / case_id
     ground_truth_dir = output_dir / "ground_truth"
     log_level = logging.INFO
 
@@ -136,13 +150,22 @@ if __name__ == "__main__":
 
     ground_truth_dir.mkdir(parents=True, exist_ok=True)
 
-    fitted_reference_mesh_file = case_dir / f"{case_id}_ssm_surface.vtp"
-    pca_file = case_dir / f"{case_id}_ssm_pca_coefficients.json"
+    compatible_case = prepare_checkpoint_compatible_lung_case(
+        source_case_dir=source_case_dir,
+        model_dir=model_dir,
+        output_dir=(output_root / "tutorial_08_lung_checkpoint" / case_id),
+        mask_dilation_mm=TCIA_4D_LUNG.mask_dilation_mm,
+        distancemap_squared_max=TCIA_4D_LUNG.distancemap_squared_max,
+        icon_weights_path=icon_distancemap_weights_path,
+        log_level=log_level,
+    )
+    fitted_reference_mesh_file = compatible_case.reference_mesh
+    pca_file = compatible_case.pca_coefficients
     for required_file in (fitted_reference_mesh_file, pca_file):
         if not required_file.exists():
             raise FileNotFoundError(
-                f"Tutorial 8 output not found: {required_file}\n"
-                "Run tutorials/tutorial_08_lung_fit_model_to_4d_patients.py first."
+                f"Checkpoint-compatible Tutorial 8 output not found: "
+                f"{required_file}"
             )
 
     # Step 1: the cohort assembles what this case is scored against.  Every
@@ -157,7 +180,7 @@ if __name__ == "__main__":
     ground_truth = cohort.assemble_ground_truth(
         case_id=case_id,
         frame_directory=data_dir,
-        fit_directory=case_dir,
+        fit_directory=output_root / "tutorial_08_lung_checkpoint" / case_id,
         cache_directory=ground_truth_dir,
     )
 